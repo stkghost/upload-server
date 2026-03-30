07c53cac7140bb60fc03761c7e65f9a24c41c8c7
Gabriel Souza <apgrl123@gmail.com>
2026-03-30 11:12:57 -0300

    ci: new configure arn

--- src/infra/http/routes/health-check.ts
+++ src/infra/http/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 
 export const getHealthCheck: FastifyPluginAsyncZod = async (server) => {
   server.get("/haelth", {}, async (request, reply) => {
-    return reply.status(200).send({ message: "Ok!" });
+    return reply.status(200).send({ message: "Health Check Ok!" });
   });
 };
