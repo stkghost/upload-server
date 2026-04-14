0a31abfcdc3c7175b1a9fbea1598d9b02383b936
Gabriel Souza <apgrl123@gmail.com>
2026-04-14 11:00:05 -0300

    fix: health check route

--- src/infra/http/routes/health-check.ts
+++ src/infra/http/routes/health-check.ts
@@ -1,7 +1,7 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 
 export const getHealthCheck: FastifyPluginAsyncZod = async (server) => {
-  server.get("/haelth", {}, async (request, reply) => {
+  server.get("/health", {}, async (request, reply) => {
     return reply.status(200).send({ message: "Health Check Success!" });
   });
 };
