f7c568e91c32fb0e818d6c8c78b35cddd2029c04
Gabriel Souza <apgrl123@gmail.com>
2026-04-14 14:53:50 -0300

    ci?:: configure deploy do ecs

--- src/infra/http/routes/health-check.ts
+++ src/infra/http/routes/health-check.ts
@@ -2,6 +2,11 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 
 export const getHealthCheck: FastifyPluginAsyncZod = async (server) => {
   server.get("/health", {}, async (request, reply) => {
-    return reply.status(200).send({ message: "Health Check Success!" });
+    return reply
+      .status(200)
+      .send({
+        message:
+          "Health Check Success agora com ECS rodando via deploy automático!",
+      });
   });
 };
